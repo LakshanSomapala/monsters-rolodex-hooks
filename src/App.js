a0737f1a57d41a86c533cb192100c6c6365e11b3
LakshanSomapala <lsachintha1992@gmail.com>
2022-06-19 15:57:56 +1000

    aaa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 
 import logo from './logo.svg';
 import CardList from './component/card-list/card-list.component'
@@ -6,15 +6,36 @@ import SearchBox from './component/search-box/search-box.component'
 import './App.css';
 
 const App = () => {
-  <div className="App">
-    <h1 className='app-title'>Monsters Rolodex</h1>
-    {/* <SearchBox
-      className='monsters-search-box'
-      placeHolder='search monsters'
-      onChangeHandler={onSearchChange}
-    ></SearchBox>
-    <CardList monsters={filteredMonsters}></CardList> */}
-  </div>
+  const [searchField, setSearchField] = useState('') // [value, setValue], string variabel
+  const [monsters, setMonsters] = useState([]); // array variable
+
+  console.log('render');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, [])
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        className='monsters-search-box'
+        placeHolder='search monsters'
+        onChangeHandler={onSearchChange}
+      ></SearchBox>
+      {/* <CardList monsters={filteredMonsters}></CardList> */}
+    </div>
+  )
 };
 
 // class App extends Component { //sadasdasdadwssa
@@ -27,21 +48,19 @@ const App = () => {
 //     };
 //     console.log('constructor');
 //   }
-//   componentDidMount() { // when react renders first time this will occuer. this happens only onces.
-//     console.log('componentDidMount');
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//       .then((response) => response.json())
-//       .then((users) =>
-//         this.setState(
-//           () => {
-//             return { monsters: users };
-//           },
-//           () => {
-//             // console.log(this.state);
-//           }
-//         )
-//       );
-//   }
+
+// componentDidMount() { // when react renders first time this will occuer. this happens only onces.
+//   console.log('componentDidMount');
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => response.json())
+//     .then((users) =>
+//       this.setState(
+//         () => {
+//           return { monsters: users };
+//         }
+//       )
+//     );
+// }
 
 //   onSearchChange = (event) => {
 //     const searchField = event.target.value.toLocaleLowerCase();
